a2ccd8cb42e3e8c76f2d79e7cf55d89392fd22c9
odisseus <odisseus@users.noreply.github.com>
2018-10-02 14:51:18 +0200

    Removed umad. Added JFR support. Edited readme.

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# scala-umad
+# scalastrap
 
-Tool for quick and simple testing changes for parallelizing Scala.
+
+A harness for testing, debugging and profiling the Scala compiler.
 
 Setup:
 
 ```
 cd <WORKSPACE>
-git clone https://github.com/pkukielka/scala-umad.git
+git clone https://github.com/VirtusLab/scalastrap.git
 git clone -b 2.13.x-parallelize https://github.com/rorygraves/scalac_perf.git
 
 cd <WORKSPACE>/scalac_perf
@@ -16,17 +17,8 @@ sbt dist/mkPack
 Usage:
 
 ```
-cd <WORKSPACE>/scala-umad
-./run.py -s <WORKSPACE>/scalac_perf/build/pack/ -- -Yparallel-phases:parser,refchecks,patmat -Yparallel-sequential
+cd <WORKSPACE>/scalastrap
+./run.py -s <WORKSPACE>/scalac_perf/build/pack/ -f dump.jfr -- -Ystatistics
 ```
-
-Umad parameters are before `--`, jvm (scalac) parameters are after. 
-
-`-Yparallel-phases` - allows to specify phases you want to run in parallel  
-`-Yparallel-sequential` - causes all units to be run in separate threads one after another (sequentally);
-this does not provide any real paralleism but ensure all violation will be detected by umad without being affected by races
-
-Some of umad parameters can be changed in the config:
-https://github.com/pkukielka/scala-umad/blob/master/umad/src/main/resources/application.conf
 
 Also `./run.py -h` gives information about additional possible parameters.
--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import argparse
 import os
@@ -12,11 +12,11 @@ from glob import glob
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--scala', dest='scala', help='Path to the Scala build dir')
-parser.add_argument('-b', '--baseline-scala', dest='baseline', default="", help='Path to the baseline Scala build dir')
 parser.add_argument('-d', '--debugPort', dest='debugPort', help='Port on which remote debugger can be attached')
 parser.add_argument('-c', '--corpus', dest='corpus', default="scala-library", help='Project to compile')
+parser.add_argument('-f', '--jfr-file', dest='jfrFile', help='Enable Java Flight Recorder and write the recorting to this file (requires Oracle JVM)')
 parser.add_argument('-p', '--config', dest='config', default=[], type=str, nargs='+',
-                    help='Config overrides (key=values paris)')
+                    help='Config overrides (key=value pairs)')
 parser.add_argument('additionalOptions', type=str, nargs="*")
 
 options = parser.parse_args()
@@ -34,7 +34,7 @@ corpus = os.path.join("corpus", options.corpus)
 sources = findFiles(os.path.join(corpus, "src"), r'\.(scala$|java$)')
 
 if len(sources) < 1:
-    print "No sources found in corpus:", corpus
+    print("No sources found in corpus:", corpus)
     sys.exit(1)
 
 jars = findFiles(os.path.join(corpus, "lib"), r'\.jar')
@@ -49,9 +49,12 @@ if options.debugPort:
     debugOptions = [
         "-J-agentlib:jdwp=transport=dt_socket,server=n,address=localhost:{},suspend=y".format(options.debugPort)]
 
-classpathSeparator = ";" if os.name == 'nt' else ":"
+jfrOptions = []
+if options.jfrFile:
+    jfrOptions = [
+        "-J-XX:+UnlockCommercialFeatures", "-J-XX:+FlightRecorder", "-J-XX:StartFlightRecording=dumponexit=true,filename={}".format(options.jfrFile)]
 
-assert subprocess.call(["mvn", "package"], cwd="umad") == 0
+classpathSeparator = ";" if os.name == 'nt' else ":"
 
 outputBase = tempfile.mkdtemp()
 scalaOutput = os.path.join(outputBase, "scala")
@@ -60,44 +63,20 @@ baselineOutput = os.path.join(outputBase, "baseline")
 os.mkdir(scalaOutput)
 os.mkdir(baselineOutput)
 
-def call_compiler(scalaLocation, output, additionalScalacOptions, additionalConfig=[]):
-    agentJar = os.path.join(".", "umad", "target", "umad-1.0-SNAPSHOT.jar")
-    configOverrides = map(lambda v: "-J-D" + v, options.config + additionalConfig)
+def call_compiler(scalaLocation, output, additionalScalacOptions):
+    configOverrides = map(lambda v: "-J-D" + v, options.config)
     timeBefore = time.time()
-    subprocess.call([
-                        os.path.join(scalaLocation, "bin", "scalac"),
-                        "-cp", classpathSeparator.join(scalaJars + jars),
-                        "-d", output,
-                        "-J-javaagent:" + agentJar,
-                        "-toolcp", agentJar
-                    ] +
-                    configOverrides +
-                    scalacOptions +
-                    sources +
-                    debugOptions +
-                    additionalScalacOptions)
+    args = ([ os.path.join(scalaLocation, "bin", "scalac"), "-cp", classpathSeparator.join(scalaJars + jars), "-d", output ] +
+        list(configOverrides) +
+        scalacOptions +
+        sources +
+        debugOptions +
+        jfrOptions +
+        additionalScalacOptions)
+    subprocess.call(args)
     return time.time() - timeBefore
-
 
 compilation_time = call_compiler(options.scala, scalaOutput, options.additionalOptions)
 
-print "Compilation done in:", compilation_time, "s"
+print("Compilation done in:", compilation_time, "s")
 
-if options.baseline != "":
-    print "Compiling baseline... "
-    base_compilation_time = call_compiler(options.baseline, baselineOutput, [],
-                                          ["umad.monitor.enabled=false", "umad.chaos.enabled=false"])
-
-    print "Baseline compilation done in:", base_compilation_time, "s (", compilation_time, "for tested scalac)"
-
-    result = subprocess.call([
-        "diff", "-r",
-        scalaOutput,
-        baselineOutput,
-    ])
-
-    if result != 0:
-        print "[ERROR] Produced binary differs!"
-        sys.exit(result)
-    else:
-        print "[SUCCESS] No differences reported!"
